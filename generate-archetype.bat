aa67d3c96820e04094d0a3fd2cdebee7626e5353
Jlarx <joh.laram@duocuc.cl>
2026-04-05 13:00:19 -0400

    actualizacion final

--- generate-archetype.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Generando Plantilla Arquetipo Maven de Donaciones...
-cd Donaciones
-..\..\mvnw archetype:create-from-project -DinteractiveMode=false
-echo.
-echo Arquetipo generado exitosamente en Donaciones\target\generated-sources\archetype
-echo Para instalar el arquetipo en tu repositorio local, corre:
-echo cd Donaciones\target\generated-sources\archetype ^&^& ..\..\..\..\mvnw install
-pause
